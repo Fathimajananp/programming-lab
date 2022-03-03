bcd9c0d742efb320d5f720097b06566e25770be5
Fathimajananp <95746148+Fathimajananp@users.noreply.github.com>
2022-03-03 10:37:03 +0530

    Add files via upload

---
+++ sphere.py
@@ -0,0 +1,8 @@
+#sphere area and perimeter
+import math
+def sphereArea(r):
+    area=4*math.pi*r*r
+    print("Area=",area)
+def sphereCircumference(r):
+    perimeter= 2*math.pi*r
+    print("Perimeter=",perimeter)
