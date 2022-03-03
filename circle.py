1a7958d9bbaf84a90668bc91d02112ae2d00bf7b
Fathimajananp <95746148+Fathimajananp@users.noreply.github.com>
2022-03-03 10:28:02 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,9 @@
+#circle area and perimeter
+import math
+def circleArea(r):
+    area=math.pi*r*r
+    print("Area=",area)
+def circlePerimeter(r):
+    perimeter= 2*math.pi*r
+    print("perimeter=",perimeter)
+
