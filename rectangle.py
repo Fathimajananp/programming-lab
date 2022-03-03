eac6505859cb1bb476bde6a2220862253c8b22b7
Fathimajananp <95746148+Fathimajananp@users.noreply.github.com>
2022-03-03 10:30:52 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+#rectangle area and perimeter
+def recArea(l,b):
+    area=l*b
+    print("Area=",area)
+def recPerimeter(l,b):
+    perimeter =2*(l+b)
+    print("perimeter=",perimeter)
+
+
+
---
+++ test.py
@@ -0,0 +1,3 @@
+def diff(a,b):
+    result=a-b
+    return result
