580fa937c35f5dbfdcaad4740b9d71d73872dfcf
Fathimajananp <95746148+Fathimajananp@users.noreply.github.com>
2022-03-05 09:33:06 +0530

    Delete Graphics directory

--- Graphics/circle.py
+++
@@ -1,9 +0,0 @@
-#circle area and perimeter
-import math
-def circleArea(r):
-    area=math.pi*r*r
-    print("Area=",area)
-def circlePerimeter(r):
-    perimeter= 2*math.pi*r
-    print("perimeter=",perimeter)
-
--- Graphics/rectangle.py
+++
@@ -1,9 +0,0 @@
-#rectangle area and perimeter
-def recArea(l,b):
-    area=l*b
-    print("Area=",area)
-def recPerimeter(l,b):
-    perimeter =2*(l+b)
-    print("perimeter=",perimeter)
-
-
