036beedf7795d61ad78379399671649a2f06b42f
Fathimajananp <95746148+Fathimajananp@users.noreply.github.com>
2022-03-05 09:31:48 +0530

    Delete cuboid.py

--- cuboid.py
+++
@@ -1,7 +0,0 @@
-#cuboid area and perimeter
-def cuboidArea(l,b,h):
-    area=2*((l*b)+(l*h)+(b*h))
-    print("Area=", area)
-def cuboidPerimeter(l,b,h):
-    perimeter=2*(l+b+h)
-    print("Perimeter=", perimeter)
